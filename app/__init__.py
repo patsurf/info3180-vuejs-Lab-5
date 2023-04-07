cbc4f9f4b614a886ed8f8baed7f5a2c8ad05a50e
patricia surf <74924633+patsurf@users.noreply.github.com>
2023-04-07 18:46:06 -0500

    Created front-end to display form using VueJS

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrations = Migrate(app, db)
 
-from app import views
+from app import views, modelsort views
