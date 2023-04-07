b48c19ee49b4d5b8ba5b4294d0524b3ad40118a6
patricia surf <74924633+patsurf@users.noreply.github.com>
2023-04-07 18:55:28 -0500

    created front-end page to display movies

--- app/views.py
+++ app/views.py
@@ -5,9 +5,14 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request, jsonify, send_file
+from app import app, db
+from app.forms import MovieForm
+from app.models import Movie
+from flask import render_template, request, jsonify, send_file, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from flask_wtf.csrf import generate_csrf
 import os
+from datetime import datetime
 
 
 ###
@@ -18,6 +23,43 @@ import os
 def index():
     return jsonify(message="This is the beginning of our API")
 
+
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    movie_form = MovieForm()
+    if movie_form.validate_on_submit():
+        title = movie_form.title.data
+        description = movie_form.description.data
+        poster = movie_form.poster.data
+        created_at = datetime.utcnow()
+        poster_filename = secure_filename(poster.filename)
+        poster.save(os.path.join(app.config['UPLOAD_FOLDER'], poster_filename))
+        new_movie = Movie(title, description, poster_filename, created_at)
+        db.session.add(new_movie)
+        db.session.commit()
+        movie_data = {
+            "message": "Movie successfully added",
+            "title": title,
+            "poster": url_for('getImage', filename=poster_filename),
+            "description": description
+        }
+        return jsonify(data=movie_data)
+    errors = form_errors(movie_form)
+    return jsonify(errors=errors)
+
+
+@app.route('/api/v1/movies', methods=['GET'])
+def add_movies():
+    movies = db.session.execute(db.select(Movie)).scalars()
+    movie_data = []
+    for movie in movies:
+        movie_data.append({
+           "id": movie.id,
+           "title": movie.title,
+           "description": movie.description,
+           "poster": url_for('getImage', filename=movie.poster)
+        })
+    return jsonify(movies=movie_data)
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -61,3 +103,13 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+@app.route("/api/v1/posters/<filename>")
+def getImage(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
+
+
+@app.route('/api/v1/csrf-token', methods=['GET'])
+def get_csrf():
+    return jsonify({'csrf_token': generate_csrf()})
